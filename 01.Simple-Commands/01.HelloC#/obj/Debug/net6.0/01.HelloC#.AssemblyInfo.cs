2e26d146dd7b6e8a12c884a969e2e97b4856f74c
Nazim Bayraktar <nab1079@hotmail.com>
2024-05-23 19:33:50 +0100

    Add 03.NumberOperations; 04.Calculations; 05.SquareOf7x7;

--- 01.Simple-Commands/01.HelloC#/obj/Debug/net6.0/01.HelloC#.AssemblyInfo.cs
+++ 01.Simple-Commands/01.HelloC#/obj/Debug/net6.0/01.HelloC#.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01.HelloC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d751fd483dd2e82b40629840ec688244dfa00d")]
 [assembly: System.Reflection.AssemblyProductAttribute("01.HelloC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01.HelloC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
