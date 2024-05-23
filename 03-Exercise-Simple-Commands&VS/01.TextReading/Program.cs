6ad6c328bd598b7fb542d54b841ee84397424555
Nazim Bayraktar <nab1079@hotmail.com>
2024-05-24 00:42:12 +0100

    New Project "03-Exercise-Simple-Commands&VS", added 01.3.Exercises-Simple-Commands.docx. In the project folder created 01.TextReading; 02.SquareArea programs

---
+++ 03-Exercise-Simple-Commands&VS/01.TextReading/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _01.TextReading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Console.WriteLine($"{text}");
+        }
+    }
+}
---
+++ 03-Exercise-Simple-Commands&VS/02.SquareArea/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _02.SquareArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input length of one side of a square
+            int length = int.Parse(Console.ReadLine());
+
+            // Calculation of area
+            int area = length * length;
+
+            // Output the result
+            Console.WriteLine($"{area}");
+        }
+    }
+}
