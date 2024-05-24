c2f6dbf8f29a24a48650b755f3e36583f5ed2b49
Nazim Bayraktar <nab1079@hotmail.com>
2024-05-24 23:59:36 +0100

    Create Project named 05-Simple-Calculations. Create program for converting usd to euro 01.Convertor-USDtoEUR; and program for four operations: 02.FourOperations;

---
+++ 05.Simple-Calculations/01.ConvertorUSDtoEUR/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.ConvertorUSDtoEUR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read a floating-point number from console (the dollars to be converted)
+            double usd = double.Parse(Console.ReadLine());
+
+            // Converting USD to EUR (fixed rate of dollars to euro: 0.88)
+            double euro = usd * 0.88;
+
+            // Output the result to the console
+            Console.WriteLine($"{euro:F2}");
+        }
+    }
+}
---
+++ 05.Simple-Calculations/02.FourOperations/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02.FourOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read two floating-point numbers: first number and second number
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+
+            // Performing 4 aritmetic operations on the given numbers: + - * /
+            double addition = firstNum + secondNum;
+            double subtraction = firstNum - secondNum;
+            double multiplication = firstNum * secondNum;
+            double division = firstNum / secondNum;
+
+            // Printing the results all formatted to the 2nd digit
+            Console.WriteLine($"{firstNum:F2} + {secondNum:F2} = {addition:F2}");
+            Console.WriteLine($"{firstNum:F2} - {secondNum:F2} = {subtraction:F2}");
+            Console.WriteLine($"{firstNum:F2} * {secondNum:F2} = {multiplication:F2}");
+            Console.WriteLine($"{firstNum:F2} / {secondNum:F2} = {division:F2}");
+        }
+    }
+}
