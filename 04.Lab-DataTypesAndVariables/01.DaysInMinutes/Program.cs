1569413a41e991b7a849ac1ed1ee27a59a279e5c
Nazim Bayraktar <nab1079@hotmail.com>
2024-05-24 11:05:30 +0100

    Create Project named 04.Lab-DataTypesAndVariables and add 02.2.Data-Types-and-Variables-Lab.docx (task conditions); Create 1st program to converting days to minutes 01.DaysToMinutes;

---
+++ 04.Lab-DataTypesAndVariables/01.DaysInMinutes/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.DaysInMinutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input a single integer (the days to be converted)
+            int days = int.Parse(Console.ReadLine());
+
+            // Converting the days to minutes (1 day = 24 hours * 60 minutes)
+            int minutes = days * 24 * 60;
+
+            // Output the result to the console
+            Console.WriteLine($"Minutes = {minutes}");
+        }
+    }
+}
