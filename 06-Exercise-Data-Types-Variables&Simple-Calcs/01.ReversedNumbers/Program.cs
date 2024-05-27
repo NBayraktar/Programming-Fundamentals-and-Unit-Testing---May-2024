7cf443b1c2afa2621331a5658cc1e562bc5de999
Nazim Bayraktar <nab1079@hotmail.com>
2024-05-27 23:24:33 +0100

    Add new Project (06-Exercise-Data-Types-Variable&Simple-Calcs), and create simple program for reading 3 numbers from the console and output the result in reversed order (01.ReversedNumbers)

---
+++ 06-Exercise-Data-Types-Variables&Simple-Calcs/01.ReversedNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.ReversedNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read 3 floating point numbers from the console
+            double firstNum = double.Parse(Console.ReadLine());
+            double secontNum = double.Parse(Console.ReadLine());
+            double thirdNum = double.Parse(Console.ReadLine());
+
+            // Output the result to console in reversed order
+            Console.WriteLine(thirdNum);
+            Console.WriteLine(secontNum);
+            Console.WriteLine(firstNum);
+        }
+    }
+}
