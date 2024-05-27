cc4bc0f70efc37c26156459fc92e1051c82d22cc
Nazim Bayraktar <nab1079@hotmail.com>
2024-05-28 00:15:52 +0100

    Added 02.3.Exrcises-Data-Types-Variables-Simple-Calculations.docx, make project name shorter, and create program to calculate centuries to years, years to days, days to hours, hours to minutes: 02.CenturiesToMinutes

---
+++ 06-ExerDataTypesVar&SimpleCalcs/02.CenturiesToMinutes/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.CenturiesToMinutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an integer number, that represents centuries for conversion
+            int century = int.Parse(Console.ReadLine());
+
+            // Converting centuries to years, years to days, days to hours, hours to minutes
+             int years = century * 100;
+            int days = (int)(years * 365.2422);
+            int hours = days * 24;
+            int minutes = hours * 60;
+
+            // Printing the result to the console
+            Console.WriteLine($"{century} centuries = {(int)years} years = {(int)days} days = {(int)hours} hours = {(int)minutes} minutes");
+        }
+    }
+}
+
