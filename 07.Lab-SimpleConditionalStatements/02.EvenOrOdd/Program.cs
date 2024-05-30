4d6838ae00801e3e92dfd6467b5704faef147c5c
Nazim Bayraktar <nab1079@hotmail.com>
2024-05-31 00:47:44 +0100

    Create program that reads integer number and checks if it even ot odd called: 02.EvenOrOdd

---
+++ 07.Lab-SimpleConditionalStatements/02.EvenOrOdd/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _02.EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input number from the console
+            int number = int.Parse(Console.ReadLine());
+
+            // Checks the number for 'even' or 'odd'
+            if (number % 2 == 0) {
+                Console.WriteLine("even");
+            } else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
