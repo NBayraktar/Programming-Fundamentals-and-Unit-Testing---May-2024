de1ff7183f2f00d24042f733861a05926e3b0a0e
Nazim Bayraktar <nab1079@hotmail.com>
2024-05-31 00:01:53 +0100

    Create project named: 07.Lab-SimpleContitionalStatements

---
+++ 01.FreezingWeather/07.Lab-SimpleConditionalStatements/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace _07.Lab_SimpleConditionalStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
