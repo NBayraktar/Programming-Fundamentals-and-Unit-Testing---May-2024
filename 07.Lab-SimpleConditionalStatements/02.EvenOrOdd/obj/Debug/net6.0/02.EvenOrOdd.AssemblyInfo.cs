801f3c3c84892848b4964999eaa21aa2c52a2f46
Nazim Bayraktar <nab1079@hotmail.com>
2024-05-31 22:02:14 +0100

    Created program that receives number from the console water temprerature in C, and checks if it is hot enough to boil the water: 06.BoilingWater

--- 07.Lab-SimpleConditionalStatements/02.EvenOrOdd/obj/Debug/net6.0/02.EvenOrOdd.AssemblyInfo.cs
+++ 07.Lab-SimpleConditionalStatements/02.EvenOrOdd/obj/Debug/net6.0/02.EvenOrOdd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02.EvenOrOdd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0028fa0b5feff020f490998bf65c885a767960fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b795fb4d8e65aa1e2b6a02f3f11f326740919ccb")]
 [assembly: System.Reflection.AssemblyProductAttribute("02.EvenOrOdd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02.EvenOrOdd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 07.Lab-SimpleConditionalStatements/06.BoilingWater/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _06.BoilingWater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input the number from the console temperature (in C)
+            int tempCel = int.Parse(Console.ReadLine());
+           
+            // Checking the water is it hot enough 
+            if (tempCel > 100)
+            {
+                Console.WriteLine("The water is boiling");
+            } 
+            else
+            {
+                Console.WriteLine("The water is not hot enough");
+            }
+        }
+    }
+}
