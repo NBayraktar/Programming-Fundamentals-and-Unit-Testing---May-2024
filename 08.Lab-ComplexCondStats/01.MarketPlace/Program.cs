9fad8ded3b1982f3918d873143a327a9a68b0c53
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-02 22:14:14 +0100

    Create program named 01.Marketplace, for showing prices for fruits during the weekday or weekend to the console

--- 08.Lab-ComplexCondStats/01.MarketPlace/Program.cs
+++ 08.Lab-ComplexCondStats/01.MarketPlace/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Read 2 string from the console
+            string fruit = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+
+            // Choosing the price for weekday or weekend
+            double weekdayBananaPrice = 2.5;
+            double weekendBananaPrice = 2.7;
+            double weekdayApplePrice = 1.3;
+            double weekendApplePrice = 1.6;
+            double weekdayKiwiPrice = 2.2;
+            double weekendKiwiPrice = 3;
+
+            if (fruit == "Banana")
+            {
+                if(dayOfWeek == "Weekday")
+                {
+                    Console.WriteLine($"{weekdayBananaPrice:F2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{weekendBananaPrice:F2}");
+                }
+            }
+            else if (fruit == "Apple")
+            {
+                if (dayOfWeek == "Weekday")
+                {
+                    Console.WriteLine($"{weekdayApplePrice:F2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{weekendApplePrice:F2}");
+                }
+            }
+            else if (fruit == "Kiwi")
+            {
+                if (dayOfWeek == "Weekday") {
+                    Console.WriteLine($"{weekdayKiwiPrice:F2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{weekendKiwiPrice:F2}");
+                }
+            }
+
         }
     }
 }
--- 08.Lab-ComplexCondStats/01.MarketPlace/obj/Debug/net6.0/01.MarketPlace.AssemblyInfo.cs
+++ 08.Lab-ComplexCondStats/01.MarketPlace/obj/Debug/net6.0/01.MarketPlace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01.MarketPlace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a154523c4377f95eb794943a3a6b19353abbe7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a241cece6c33787c8f4c9b61fc7f9153195ab145")]
 [assembly: System.Reflection.AssemblyProductAttribute("01.MarketPlace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01.MarketPlace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
