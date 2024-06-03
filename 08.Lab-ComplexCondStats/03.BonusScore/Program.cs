78e57f8ca231ea8da856138e06bb4d585eb4a07e
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-04 00:14:06 +0100

    Created program called 03.BonusScore, wich reads points from console (integer number) and adds bonus points to given points

---
+++ 08.Lab-ComplexCondStats/03.BonusScore/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _03.BonusScore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reading points (integer number) from the console
+            int points = int.Parse(Console.ReadLine());
+
+            // Adding bonus points to given points
+            if (points >= 0 && points <= 3)
+            {
+                points += 5;
+                Console.WriteLine(points);
+            }
+            else if (points >= 4 && points <= 6)
+            {
+                points += 15;
+                Console.WriteLine(points);
+            }
+            else if (points >= 7 && points <= 9)
+            {
+                points += 20;
+                Console.WriteLine(points);
+            }
+        }
+
+    }
+}
