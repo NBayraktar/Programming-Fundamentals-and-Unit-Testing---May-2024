8631f74bdb51f7acb006874bcdff9dd5776f4c34
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-05 00:54:50 +0100

    Created program named: 09.Cinema, wich reads type of the cinema, rows and seats from the console and calculate the total cost of the tickets

--- 08.Lab-ComplexCondStats/02.LargestNumberOutOfThree/obj/Debug/net6.0/02.LargestNumberOutOfThree.AssemblyInfo.cs
+++ 08.Lab-ComplexCondStats/02.LargestNumberOutOfThree/obj/Debug/net6.0/02.LargestNumberOutOfThree.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("02.LargestNumberOutOfThree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd7813c2ae44f4b9187ac533f2b2e9a50c831f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6597daeecffb6be38805e1b9b85c49639fc7480")]
 [assembly: System.Reflection.AssemblyProductAttribute("02.LargestNumberOutOfThree")]
 [assembly: System.Reflection.AssemblyTitleAttribute("02.LargestNumberOutOfThree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 08.Lab-ComplexCondStats/09.Cinema/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _09.Cinema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reading from the console the type of the movie, count of the rows, and count of the seats per row (integer numbers)
+            string movieType = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int seats = int.Parse(Console.ReadLine());
+
+            // Calculating the total price of the all seats
+            double premierePrice = 12;
+            double normal = 7.5;
+            double discount = 5;
+            if (movieType == "Premiere")
+            {
+                double ticketCost = rows * seats * premierePrice;
+                Console.WriteLine($"{ticketCost:F2}");
+            }
+            else if (movieType == "Normal")
+            {
+                double ticketCost = rows * seats * normal;
+                Console.WriteLine($"{ticketCost:F2}");
+            }
+            else
+            {
+                double ticketCost = rows * seats * discount;
+                Console.WriteLine($"{ticketCost:F2}");
+            }
+
+        }
+    }
+}
