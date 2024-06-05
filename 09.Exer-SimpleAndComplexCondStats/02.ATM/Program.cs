5a1fd9660777dd33258262bf08b82da6d5dee148
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-05 19:03:07 +0100

    Created program called 02.ATM wich simulate an ATM withdrawal

---
+++ 09.Exer-SimpleAndComplexCondStats/02.ATM/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _02.ATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Reads 3 integer numbers which represents the following values: balance, withdraw and limit
+            int balance = int.Parse(Console.ReadLine());
+            int withdraw = int.Parse(Console.ReadLine());
+            int limit = int.Parse(Console.ReadLine());
+
+            // Output the result to the console, based on the given input parameters
+            if (balance > withdraw)
+            {
+                if (withdraw < limit)
+                {
+                    Console.WriteLine("The withdraw was successful.");
+                }
+                else
+                {
+                    Console.WriteLine("The limit was exceeded.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Insufficient availability.");
+            }
+        }
+    }
+}
