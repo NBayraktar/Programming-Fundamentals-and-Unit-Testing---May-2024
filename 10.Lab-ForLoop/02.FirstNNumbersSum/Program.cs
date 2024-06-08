34e1acfa3d5fceb508742592683f89a42ea6a147
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-08 23:36:39 +0100

    Created program which sums the numbers 1...n and output the result to the console. Program name is: 02.FirstNnumbersSum

---
+++ 10.Lab-ForLoop/02.FirstNNumbersSum/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.FirstNNumbersSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an integer number 'n' from the console
+            int num = int.Parse(Console.ReadLine());
+
+            // Sums all numbers from 1 to n and output the result to the console
+            int sum = 1; 
+            Console.Write(1);
+            for (int i = 2; i <= num; i++)
+            {
+                Console.Write("+" + i);
+                sum += i;
+            }
+            Console.WriteLine("=" + sum);
+        }
+    }
+}
