1bfa45b958e94f9895c14afa5fd5928be3b62167
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-08 23:06:25 +0100

    Created program that prints numbers in given range called: 01.NumbersInRange

---
+++ 10.Lab-ForLoop/01.NumbersInRange/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.NumbersInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read 2 integer numbers from the console
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            // Print the numbers in the given range (include start and end number), each on the new line
+            for (int i = num1; i <= num2; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
