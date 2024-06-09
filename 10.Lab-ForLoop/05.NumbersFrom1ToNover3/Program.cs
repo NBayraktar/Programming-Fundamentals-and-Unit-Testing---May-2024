c30ead1c351f9275bad6f03d693097a6f6d6df16
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-09 22:54:14 +0100

    Created program that prints numbers in given range over 3 to the console. Program called: 05.NumbersFrom1ToNover3.

---
+++ 10.Lab-ForLoop/05.NumbersFrom1ToNover3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _05.NumbersFrom1ToNover3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read integer number n - end of the range
+            int number = int.Parse(Console.ReadLine());
+
+            // Print all numbers from 1 to n, over 3 (inclusively)
+            for (int i = 1; i <= number; i += 3) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
