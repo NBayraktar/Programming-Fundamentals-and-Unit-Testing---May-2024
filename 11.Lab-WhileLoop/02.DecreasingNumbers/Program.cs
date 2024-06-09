de81554d7043085479e2985c3996f8c4bd6e62b9
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-09 23:50:22 +0100

    Created program that reads intege number and then prints numbers from N down to 1. Program called:02.DecreasingNumbers.

---
+++ 11.Lab-WhileLoop/02.DecreasingNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _02.DecreasingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an integer number N from console
+            int number = int.Parse(Console.ReadLine());
+
+            // Print the numbers from N down to 1(inclusively), each on separate line
+            while (number >= 1)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+        }
+    }
+}
