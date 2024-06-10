bdc65965c897be3de7cf34d1a5bc5575cb363be7
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-10 23:02:31 +0100

    Created program that reads numbers from the console until an odd number is entered, and output the result to the console. Program called: 03.OddNumber.

---
+++ 11.Lab-WhileLoop/03.OddNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _03.OddNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read numbers from the console until an odd number is entered and print result
+            int num = int.Parse(Console.ReadLine());
+
+            while (num % 2 == 0)
+            {
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(num);
+        }
+    }
+}
