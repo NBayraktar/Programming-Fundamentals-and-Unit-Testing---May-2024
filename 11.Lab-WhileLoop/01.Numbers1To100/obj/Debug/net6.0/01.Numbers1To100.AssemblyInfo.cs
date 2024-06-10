ec87ff731ea4db931775e0f2f8be1ec75e8ef4ce
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-10 23:57:39 +0100

    Create program called: 05.SumDigits, wich sums the digits of given number and shows result to the console

--- 11.Lab-WhileLoop/01.Numbers1To100/obj/Debug/net6.0/01.Numbers1To100.AssemblyInfo.cs
+++ 11.Lab-WhileLoop/01.Numbers1To100/obj/Debug/net6.0/01.Numbers1To100.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01.Numbers1To100")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc65965c897be3de7cf34d1a5bc5575cb363be7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d5db7b6982fe6423e556549a895d25c56cdfd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("01.Numbers1To100")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01.Numbers1To100")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 11.Lab-WhileLoop/05.SumDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _05.SumDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an integer positive number from the console
+            int num = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            while (num != 0)
+            {
+                int lastDigit = num % 10;
+                sum += lastDigit;
+                num /= 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
