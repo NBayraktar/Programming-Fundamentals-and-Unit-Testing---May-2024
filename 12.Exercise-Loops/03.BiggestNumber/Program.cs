4469f148287decfb23e28484dac6f89b91225f94
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-12 18:10:31 +0100

    Created program to find the biggest among given n numbers. Program called: 03.BiggestNumber

---
+++ 12.Exercise-Loops/03.BiggestNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _03.BiggestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an integer number n (the amount of input numbers) and n integer numbers from the console
+            int inputAmount = int.Parse(Console.ReadLine());
+
+            // Read n integer numbers from the console and find the biggest number
+            int biggestNum = int.MinValue; 
+            for (int i = 0; i < inputAmount; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (currentNum > biggestNum)
+                {
+                    biggestNum = currentNum;
+                }
+            }
+            Console.WriteLine(biggestNum);
+        }
+    }
+}
