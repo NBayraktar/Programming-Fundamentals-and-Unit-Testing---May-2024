fbdcca9ffdb41168344670f493156428f98b5686
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-12 16:29:58 +0100

    Created program that prints a multiplication table called: 02.MultiplicationTable.

---
+++ 12.Exercise-Loops/02.MultiplicationTable/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _02.MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an integer number n from the console
+            int num = int.Parse(Console.ReadLine());
+
+            // Print a multiplication table of size 10 for given integer n
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = num * i;
+                Console.WriteLine($"{num } x {i} = {result}");
+            }
+        }
+    }
+}
