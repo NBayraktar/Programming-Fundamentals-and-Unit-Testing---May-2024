db92a9400ca92f51474ec41c65b774b35b6a2cdd
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-13 22:12:03 +0100

    Creating program called: 05.DivisionTo2,3And4

--- 12.Exercise-Loops/04.VowelSum/obj/Debug/net6.0/04.VowelSum.AssemblyInfo.cs
+++ 12.Exercise-Loops/04.VowelSum/obj/Debug/net6.0/04.VowelSum.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("04.VowelSum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4469f148287decfb23e28484dac6f89b91225f94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+684adf609df1e88043c6539cd0df1089255faf48")]
 [assembly: System.Reflection.AssemblyProductAttribute("04.VowelSum")]
 [assembly: System.Reflection.AssemblyTitleAttribute("04.VowelSum")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 12.Exercise-Loops/05.DivisionT23And4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _05.DivisionT23And4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an integer number N from the console
+            int num = int.Parse(Console.ReadLine());
+
+
+            // Find in percentages how many of these integers can divide without a remainder to numbers 2, 3 and 4
+            int divisible2 = 0;
+            int divisible3 = 0;
+            int divisible4 = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (currentNum % 2 == 0)
+                {
+                    divisible2++;
+                }
+                if (currentNum % 3 == 0)
+                {
+                    divisible3++;
+                }
+                if (currentNum % 4 == 0)
+                {
+                    divisible4++;
+                }
+            }
+
+
+        }
+    }
+}
