d16849ee612aec831d2e54f64a7065f97f71c3bd
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-14 23:52:01 +0100

    Created program that calculate an account balance. Program called: 08.AccountBalance

--- 12.Exercise-Loops/06.SpecialNumber/obj/Debug/net6.0/06.SpecialNumber.AssemblyInfo.cs
+++ 12.Exercise-Loops/06.SpecialNumber/obj/Debug/net6.0/06.SpecialNumber.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("06.SpecialNumber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5711fa1fa06b2c281266194c7cb04d92d1e1d1d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42761d45ea3472289f630f286d09a4ac413fd09a")]
 [assembly: System.Reflection.AssemblyProductAttribute("06.SpecialNumber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("06.SpecialNumber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 12.Exercise-Loops/08.AccountBalance/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _08.AccountBalance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read a sequence of incomes / expenses, until "End" is read
+
+            double balance = 0;
+            while (true)
+            {
+                double money = double.Parse(Console.ReadLine());
+
+                if (money > 0)
+                {
+                    balance += money;
+                    Console.WriteLine($"Increase: {money:F2}");
+                }
+                else if (money < 0)
+                {
+                    balance = balance - Math.Abs(money);
+                    Console.WriteLine($"Decrease: {Math.Abs(money):F2}");
+                    string end = Console.ReadLine();
+                    break;
+                }
+            }
+            Console.WriteLine($"Balance: {balance:F2}");
+
+        }
+    }
+}
