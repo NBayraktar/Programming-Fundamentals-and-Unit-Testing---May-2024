425931fe4031c43061709552bb504850b36947ee
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-18 00:16:42 +0100

    Created program wich print a triangle of stars to the console. Program called: 03.TriangleOfStars.

---
+++ 13.Lab-NestedLoops/03.TriangleofStars/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03.TriangleofStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the size (integer number) of a triangle from the console
+            int size = int.Parse(Console.ReadLine());
+
+            // Print triangle of stars to the console
+            for (int row = 1; row <= size; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write($"*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
