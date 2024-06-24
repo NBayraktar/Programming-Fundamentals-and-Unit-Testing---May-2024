d1da691adff219f459da95a932a8e3f31657a6d1
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-24 23:27:57 +0100

    Created program called: 02.Grades. Program reads grade a floating-point number from the console, and calling method that receives a grade between 2.00 and 6.00. Program then prints corresponding grade in words.

---
+++ 14.Lab-Methods/02.Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _02.Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read a grad (floating-point number) from the console
+            double number = double.Parse(Console.ReadLine());
+            Grades(number);
+        }
+
+        static void Grades(double num)
+        {
+            if (num >= 2.00 && num <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (num >= 3.00 && num <= 3.49)
+            {
+                Console.WriteLine("Average");
+            }
+            else if (num >= 3.50 && num <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (num >= 4.50 && num <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
