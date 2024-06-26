170e249daf8469bde84a0a46823540f3d7ec69a0
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-26 23:36:34 +0100

    Created Program wich reads an integer number N from the console. Generates all possible passwords consisting of the following pards: First is an even number [2...N], second digit is an odd number in the range [1...N] and the third is the product of the first two. Program named: 01.StupidPasswords.

---
+++ 15.Exer-NestLoopsAndMeths/01.StupidPasswords/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace _01.StupidPasswords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an integer number N from the console
+            int number = int.Parse(Console.ReadLine());
+
+            // Generates all possible passwords consisting of the following 3 parts:
+            // The first part is an even number in the range[2…N]
+            // The second digit is an odd number in the range [1…N]
+            // The second digit is an odd number in the range [1…N]
+            for (int even = 2; even <= number; even += 2)
+            {
+                for (int odd = 1; odd <= number; odd += 2)
+                {
+                    int product = even * odd;
+                    Console.Write($"{even}{odd}{product} ");
+                }
+            }
+        }
+    }
+}
