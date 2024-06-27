0d7d60549ddd8dc2216058534ff3f4f4a7faa65d
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-27 22:14:49 +0100

    Create program that generates PIN codes following certain rules. Program named: 03.UniquePINCodes.

--- 15.Exer-NestLoopsAndMeths/01.StupidPasswords/obj/Debug/net6.0/01.StupidPasswords.AssemblyInfo.cs
+++ 15.Exer-NestLoopsAndMeths/01.StupidPasswords/obj/Debug/net6.0/01.StupidPasswords.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01.StupidPasswords")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e210039dc2f5608178cf4d86091251afcf29df7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b73946a30b7d5ff1c2c9a4848c6a899a60f9eab")]
 [assembly: System.Reflection.AssemblyProductAttribute("01.StupidPasswords")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01.StupidPasswords")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
