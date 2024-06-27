7b73946a30b7d5ff1c2c9a4848c6a899a60f9eab
Nazim Bayraktar <nab1079@hotmail.com>
2024-06-27 20:47:06 +0100

    Created program called: 02.MagicNumbers. It reads an integer number N from the console and finds 3-digit magic numbers.

---
+++ 15.Exer-NestLoopsAndMeths/02.MagicNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02.MagicNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an integer number N from the console
+            int number = int.Parse(Console.ReadLine());
+
+            // Find all 3 digit magic numbers
+            for (int a = 1; a <= number; a++)
+            {
+                for (int b = 1; b <= number; b++)
+                {
+                    for (int c = 1; c <= number; c++)
+                    {
+                        if (a >= 10 || b >= 10 || c >= 10)
+                        {
+                            continue;
+                        }
+
+                        int magicNum = a * b * c;
+                        if (number == magicNum)
+                        {
+                            Console.Write($"{a}{b}{c} ");
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
