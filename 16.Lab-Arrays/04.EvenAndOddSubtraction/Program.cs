8ed360d245f842c1a321e015f542ca43fe984426
Nazim Bayraktar <nab1079@hotmail.com>
2024-07-01 23:12:10 +0100

    Created program called: 04.EvenAndOddSubtraction. Program reads an array of integers from a single line separated with a space, and calculates the difference between the sum of the even and the sum of odd numbers.

---
+++ 16.Lab-Arrays/04.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _04.EvenAndOddSubtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an array of integers (from a single line separated with a space)
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            // Calculate the differece between the sum of the even and the sum of the odd numbers in an array
+            int evenNum = 0;
+            int oddNum = 0;
+            
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    evenNum += number;
+                }
+                else
+                {
+                    oddNum += number;
+                }
+            }
+            int difference = evenNum - oddNum;
+            Console.WriteLine(difference);
+        }
+    }
+}
