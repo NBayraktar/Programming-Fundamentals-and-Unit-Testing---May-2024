96e19a493689bc04029bf7b8ac001c9e8f803ba3
Nazim Bayraktar <nab1079@hotmail.com>
2024-07-01 14:44:04 +0100

    Created program that sums of all elements in the given array. Reads an array of integers from the console (single line separated with a space). Program named: 02.SumAnArray.

---
+++ 16.Lab-Arrays/02.SumAnArray/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.SumAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an array of integers (from a single line separated with a space)
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            // Print the sum of all elements in the given array to the console
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
