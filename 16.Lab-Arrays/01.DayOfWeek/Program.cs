9c4f86fbd5b3a893b1c70539aa4ad25098e0cea4
Nazim Bayraktar <nab1079@hotmail.com>
2024-07-01 12:51:42 +0100

    Added new project called: 16.Lab-Arrays. Created program called: 01.DayOfWeek, wich reads integer number from the console (day of week), checks if the day is in range and prints message to the console.

---
+++ 16.Lab-Arrays/01.DayOfWeek/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.DayOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an integer number (day) from the console
+            int day = int.Parse(Console.ReadLine());
+
+            // Array of days
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            // Check the day is it in range and print the message to the console
+            if (day < 1 || day > 7)
+            {
+                Console.WriteLine("Invalid day!");
+                return;
+            }
+            Console.WriteLine(days[day - 1]);
+        }
+    }
+}
