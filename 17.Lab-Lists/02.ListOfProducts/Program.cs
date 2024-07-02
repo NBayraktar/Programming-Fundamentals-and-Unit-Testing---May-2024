50c87742994de2c8d059b2a15bd0e0b1b85b1953
Nazim Bayraktar <nab1079@hotmail.com>
2024-07-02 22:04:45 +0100

    Created program named: 02.ListOfProducts. Program reads an integer number n and n lines of product. After program prints a numbered list of all the products ordered by name to the console.

---
+++ 17.Lab-Lists/02.ListOfProducts/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02.ListOfProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an integer number n and n lines of products
+            int number = int.Parse(Console.ReadLine());
+
+            List<string> products = new List<string>();
+            for (int i = 0; i < number; i++) {
+                products.Add(Console.ReadLine());
+            }
+
+            // Print a numbered list of all the products ordered by name
+            products.Sort();
+            for(int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }           
+        }
+    }
+}
