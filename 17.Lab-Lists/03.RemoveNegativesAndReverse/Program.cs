7fe7a516a2ad9617ca4de2bd5f406a51a5ea3fa5
Nazim Bayraktar <nab1079@hotmail.com>
2024-07-03 23:49:09 +0100

    Created program wich reads a list of integers. Removes all negative numbers from it and prints remaining elements in reverse order. If there are no elements in the list, prints (empty) to the console. Program named: 03.RemoveNegativesAndReverse.

---
+++ 17.Lab-Lists/03.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _03.RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read List of integers
+            List<int> numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+
+            // Remove all negative numbers from in and print the remaining elements in reverse order. If there are no elements left in the list, print "empty".
+            List<int> positiveNumbers = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (number > 0)
+                {
+                    positiveNumbers.Add(number);
+                }
+            }
+
+            if (positiveNumbers.Count != 0)
+            {
+                positiveNumbers.Reverse();
+                Console.WriteLine($"{string.Join(" ", positiveNumbers)}");
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
