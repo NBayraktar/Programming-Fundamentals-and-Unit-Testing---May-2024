2f07604a60efad7533b982717ae1a8ed80ba5519
Nazim Bayraktar <nab1079@hotmail.com>
2024-07-04 21:38:36 +0100

    Created program with name: 04.ListManipulationBasics. Program reads a list of integers and then until receives "end" command continues to recevies different commands (Add, Remove, RemoveAt, Inster). When receives "end" command program prints to the console the final state of the list separated by spaces.

---
+++ 17.Lab-Lists/04.ListManipulationBasics/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _04.ListManipulationBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read a list of integers from the console
+            List<int> numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+
+            string line = "";
+            while ((line = Console.ReadLine()) != "end")
+            {
+                string[] command = line.Split();
+                if (command[0] == "Add")
+                {
+                    numbers.Add(int.Parse(command[1]));
+                }
+                else if (command[0] == "Remove")
+                {
+                    numbers.Remove(int.Parse(command[1]));
+                }
+                else if (command[0] == "RemoveAt")
+                {
+                    numbers.RemoveAt(int.Parse(command[1]));
+                }
+                else if (command[0] == "Insert")
+                {
+                    numbers.Insert(int.Parse(command[2]), int.Parse(command[1]));
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+    }
+}
