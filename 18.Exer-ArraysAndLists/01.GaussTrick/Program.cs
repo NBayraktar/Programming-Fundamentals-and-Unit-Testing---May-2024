a39ca12e02243a79810405c7617500b28cdf4efb
Nazim Bayraktar <nab1079@hotmail.com>
2024-07-12 23:58:19 +0100

    Created Program called: 01.GaussTrick.

--- 18.Exer-ArraysAndLists/01.GaussTrick/Program.cs
+++ 18.Exer-ArraysAndLists/01.GaussTrick/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Read a sequence with integer numbers from the console
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+            // Sum all numbers in a list in the following order:first + last, first + 1 + last - 1, first + 2 + last - 2, … first + n, last – n
+            List<int>output = new List<int>();
+            int iterations = numbers.Length;
+            for (int i = 0; i < iterations; i++) {
+                int currentNum = numbers[i] + numbers.Length - 1 - i;
+                output.Add(currentNum);
+            }
+            if (numbers.Length % 2 != 0) { 
+                int index = numbers.Length / 2;
+                output.Add(numbers[index]);
+            }
+            Console.WriteLine(string.Join(" ", output));
         }
     }
 }
