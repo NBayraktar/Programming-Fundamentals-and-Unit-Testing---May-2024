c6ee0fd6e00988cc50b846f7a028e9ebe8c5f9cb
Nazim Bayraktar <nab1079@hotmail.com>
2024-07-14 21:45:27 +0100

    Created program named: 02.CondenseArrayToNumber. Program reads a sequence of integer numbers from the console. Condense them by summing adjacent couples of elements until a single integer is obtained.

---
+++ 18.Exer-ArraysAndLists/02.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _02.CondenseArrayToNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read a sequence of integer numbers from the console
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            // Condense them by summing adjacent couples of elements until a single integer is obtainded, then print the result to the console
+            while (numbers.Length > 1)
+            {
+                int[] condensedNums = new int[numbers.Length - 1];
+                for (int i = 0; i < numbers.Length - 1; i++) {
+                    int sumAdjasentNums = numbers[i] + numbers[i + 1];
+                    condensedNums[i] = sumAdjasentNums;
+                }
+                numbers = condensedNums;
+            }
+            Console.WriteLine(numbers[0]);
+        }
+    }
+}
