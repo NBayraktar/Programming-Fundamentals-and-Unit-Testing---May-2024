13b9c72e91d15c1e9c93b0bbaab3c4dddf2549d8
Nazim Bayraktar <nab1079@hotmail.com>
2024-08-05 23:54:29 +0100

    Created program that prints triangle of numbers. Program reads integer number N from the console, calls method for printing numbers. Program called: 03.PrintingTriangle.

---
+++ 00.Practice/03.PrintingTriangle/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _03.PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an integer number N from the console
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{j} ");
+                    if (j == number)
+                    {
+                        PrintingTriangle(number);
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            static void PrintingTriangle(int number)
+            {
+                Console.WriteLine();
+                for (int i = number - 1; i > 0; i--)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write($"{j} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
