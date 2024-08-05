b7ded22fa00c71e7925b5dbf11a8e7646ff0152a
Nazim Bayraktar <nab1079@hotmail.com>
2024-08-05 19:19:58 +0100

    Create new proget file: 00.Practice. Add a program named 01.SignOfIntNums(Methods), which reads a integer number from the console and call a method that prints the sign of an entered number N and prints the message to the console.

---
+++ 00.Practice/Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            PrintSing(number);
+
+            static void PrintSing(int number) {
+                if (number > 0)
+                {
+                    Console.WriteLine($"The number {number} is positive.");
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine($"The number {number} is negative.");
+                }
+                else {
+                    Console.WriteLine($"The number {number} is zero.");
+                }
+            }
+        }
+    }
+}
